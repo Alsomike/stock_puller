df4cb04cce8c4b3569beb99deecbb5f3e4c075a9
Mike P <mpellegrino1010@gmail.com>
2021-01-04 18:49:33 -0500

    Stocks_Excel.py now works using trial lists

--- Stocks_Excel.py
+++ Stocks_Excel.py
@@ -4,14 +4,19 @@ import time
 trial_stocks = ['stock1', 'stock2', 'stock3', 'stock4', 'stock5']
 trial_prices = ['price1', 'price2', 'price3', 'price4', 'price5']
 
-SR_wb = xl.load_workbook('\\Users\\mpell\\OneDrive\\Documents\\PythonProjects\\Git\\Stock_Records.xlsx')
-#loads excel file using where it lies in the directory
-SR_Sheet = SR_wb["Sheet"]
-#finds sheet names "Sheet" in wb and gives it a python element names SR_Sheet
-now = time.strftime("%x")
-#establishes variable now that gives us the current date
-SR_Sheet['A1'].value = now
-#adds the current date to the first cell 
-SR_wb.save('Stock_Records.xlsx')
-#saves changes made to excel file
+now = time.strftime("%x") #establishes variable now that gives us the current date
 
+SR_wb = xl.load_workbook('\\Users\\mpell\\OneDrive\\Documents\\PythonProjects\\Git\\Stock_Records.xlsx') #loads excel file using where it lies in the directory
+
+SR_Sheet = SR_wb.create_sheet() #creates a new sheet within the SR_wb
+
+SR_Sheet['A1'].value = now #adds the current date to the first cell
+SR_Sheet['C1'].value = 'Ticker'
+SR_Sheet['D1'].value = 'Price'
+
+for stocks in range(0, len(trial_stocks)):
+	SR_Sheet.cell(row = stocks+2,column = 3).value=trial_stocks[stocks]
+for prices in range(0, len(trial_stocks)):
+	SR_Sheet.cell(row = prices+2,column = 4).value=trial_prices[prices]
+
+SR_wb.save('Stock_Records.xlsx') #saves changes made to excel file
