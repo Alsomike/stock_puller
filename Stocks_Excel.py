c8ea19b48ce2d82aa9de65b9a3c213ce01ed9cea
Mike P <mpellegrino1010@gmail.com>
2021-01-04 14:08:06 -0500

    clarifications for Stocks_Excel.py

--- Stocks_Excel.py
+++ Stocks_Excel.py
@@ -5,12 +5,13 @@ trial_stocks = ['stock1', 'stock2', 'stock3', 'stock4', 'stock5']
 trial_prices = ['price1', 'price2', 'price3', 'price4', 'price5']
 
 SR_wb = xl.load_workbook('\\Users\\mpell\\OneDrive\\Documents\\PythonProjects\\Git\\Stock_Records.xlsx')
+#loads excel file using where it lies in the directory
 SR_Sheet = SR_wb["Sheet"]
-
-SR_Sheet = SR_wb['Sheet']
+#finds sheet names "Sheet" in wb and gives it a python element names SR_Sheet
 now = time.strftime("%x")
+#establishes variable now that gives us the current date
 SR_Sheet['A1'].value = now
-
+#adds the current date to the first cell 
 SR_wb.save('Stock_Records.xlsx')
-
+#saves changes made to excel file
 
